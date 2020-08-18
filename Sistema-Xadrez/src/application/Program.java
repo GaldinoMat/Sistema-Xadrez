03ae7961407a4ebdaf4733c116d70cc67066026c
GaldinoMat <51252943+GaldinoMat@users.noreply.github.com>
2020-08-18 13:05:55 -0300

    Special move: promotion

--- Sistema-Xadrez/src/application/Program.java
+++ Sistema-Xadrez/src/application/Program.java
@@ -34,6 +34,13 @@ public class Program {
 				if (capturedPiece != null) {
 					capturedPieces.add(capturedPiece);
 				}
+				
+				if (match.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					match.replacePromotedPiece(type);
+				}
+				
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
